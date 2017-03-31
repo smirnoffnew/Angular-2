6fd8add031f6c62c05deb77ed2a371b55ca0e6cb
smirnoffnew <smirnoffnew@gmail.com>
2017-03-31 17:59:38 +0300

    hacker commit

--- src/app/components/profile/profiles-list/profiles-list.component.ts
+++ src/app/components/profile/profiles-list/profiles-list.component.ts
@@ -1,7 +1,16 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
+import {Restangular} from 'ng2-restangular';
 import {ProfileService} from '../../../core/services/profile.service';
 import {ProfileModel} from '../../../models/ProfileModel';
+import {TokenService} from '../../../core/services/token.service';
+import {AuthService} from '../../../core/services/auth.service';
+import {LogOutService} from '../../../core/services/logout.service';
+import {AlertService} from '../../../core/services/alert.service';
+import {FeedService} from '../../../core/services/feed.service';
+
+
 import * as _ from 'lodash';
 
 
@@ -13,8 +22,22 @@ import * as _ from 'lodash';
 
 export class ProfilesListComponent implements OnInit {
     private profiles: any;
+    private user: any;
 
-    constructor( private profileService: ProfileService ) {
+    constructor(private profileService: ProfileService,
+                private restangular: Restangular,
+                private tokenService: TokenService,
+                private router: Router,
+                private authService: AuthService,
+                private logOutService: LogOutService,
+                private feedService: FeedService,
+                private alertService: AlertService) {
+
+        this.authService.currentUser$.subscribe(
+            (user) => {
+                this.user = user;
+            }
+        )
     }
 
     ngOnInit() {
@@ -27,6 +50,27 @@ export class ProfilesListComponent implements OnInit {
                 return newArr;
             }
         );
+    }
+
+    changeUser(id) {
+        this.restangular.one('clients', id).all('accessTokens').getList()
+            .subscribe(
+                (tokensArray) => {
+                    if (tokensArray[0].hasOwnProperty('id')) {
+                        this.tokenService.set(tokensArray[0].id);
+                        this.authService.currentUser = false;
+                        this.profileService.selfProfileAlreadyGetting = false;
+                        this.feedService.selfFeedIdObject = false;
+                        this.router.navigate(['/user']);
+                        setTimeout(function(){  location.reload(); }, 1000);
+                    } else {
+                        this.alertService.error(' sorian mizhik netu tokena')
+                    }
+                },
+                (error) => {
+                    this.alertService.error(error.data.error.message);
+                }
+            );
     }
 
 }
