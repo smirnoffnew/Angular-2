6984b6fbf11d9b5c5d8a0416e6d4b94aca125afd
smirnoffnew <smirnoffnew@gmail.com>
2017-04-04 13:01:17 +0300

    Data should be displayed in readable way

--- src/app/shared/pipes/keyAndValueOfObject.pipe.ts
+++ src/app/shared/pipes/keyAndValueOfObject.pipe.ts
@@ -8,7 +8,7 @@ export class KeyAndValueOfObject implements PipeTransform {
         for (let key in value) {
             if (key !== 'image') {
                 if (key == 'birthday') {
-                    keys.push({key: key, value: new Date(value[key])});
+                    keys.push({key: key, value: this.DateFormater( new Date(value[key]) ) });
                 } else {
                     keys.push({key: key, value: value[key]});
                 }
@@ -16,4 +16,15 @@ export class KeyAndValueOfObject implements PipeTransform {
         }
         return keys;
     }
+
+    DateFormater(date) {
+        var values = [ date.getDate(), date.getMonth() + 1 ];
+        for( var id in values ) {
+            values[ id ] = values[ id ].toString().replace( /^([0-9])$/, '0$1' );
+        }
+        return values[ 0 ]+'.'+values[ 1 ]+'.'+date.getFullYear();
+    }
 }
+
+
+
